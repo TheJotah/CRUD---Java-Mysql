cea2561ac59b462cf67c5c61469b5fa2e956acee
jotaz <jotaz@JOTA>
2023-10-31 22:23:38 -0300

    CRUD_Java+Mysql_0

---
+++ crudemjava/src/br/com/agenda/factory/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package br.com.agenda.factory;
+
+public class ConnectionFactory {
+	
+	//Nome do usuário do mysql
+	private static final String USERNAME = "Jota";
+	
+	//Senha do banco
+	private static final String PASSWORD = "25416688";
+	
+	//Caminho do banco de dados, porta
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:";
+
+}
